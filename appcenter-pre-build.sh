19907c0dda1a48f11d320ada6f2c262dd3f366f4
Andac Ozcan <andac.ozcan@bazaarvoice.com>
2021-08-12 11:44:00 +0300

    Change mail to mailx for attachment support

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -23,11 +23,11 @@ function _catch {
     if [ "$AGENT_JOBSTATUS" == "Succeeded" ];
     then
         echo "e2e Tests Success!"
-        echo -e ${SUCCESS_BODY} ${build_url} | mail -a /reports/html/results.html -s "${SUBJECT} - Success!" ${TO_ADDRESS}
+        echo -e ${SUCCESS_BODY} ${build_url} | mailx -a /reports/html/results.html -s "${SUBJECT} - Success!" ${TO_ADDRESS}
         echo "success mail sent"
     else
         echo "e2e Tests Failed!"
-        echo -e ${FAILURE_BODY} ${build_url} | mail -a /reports/html/results.html -s "${SUBJECT} - Failed!" ${TO_ADDRESS}
+        echo -e ${FAILURE_BODY} ${build_url} | mailx -a /reports/html/results.html -s "${SUBJECT} - Failed!" ${TO_ADDRESS}
         echo "failure mail sent"
     fi
 }
